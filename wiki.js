ecc982768b5221a644c5416e62e004d0da6fe2d7
Rachel Cohen <rfc@alumni.duke.edu>
2017-03-13 11:40:59 -0400

    thru sequelize essentials

--- wiki.js
+++ wiki.js
@@ -1,19 +1,32 @@
 const express = require('express');
-//external libraries 
+//external libraries
 const morgan = require('morgan')
 const nunjucks = require('nunjucks');
 const routes = require("./routes");
 const wiki = express();
+const models = require('./models/index');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
 // instance, which we'll want to use to add Markdown support later.
 var env = nunjucks.configure('views', {noCache: true});
 // have res.render work with html files
-app.set('view engine', 'html');
+wiki.set('view engine', 'html');
 // when res.render works with html files, have it use nunjucks to do so
-app.engine('html', nunjucks.render);
+wiki.engine('html', nunjucks.render);
+wiki.use(express.static(path.join(__dirname, 'public')));
 
+wiki.use(bodyParser.json());
+wiki.use(bodyParser.urlencoded({extended: true}));
 
-//listen on a port 
-//app.use to register middleware
-//body parser 
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    wiki.listen(3000, function () {
+        console.log('Server is listening on port 3001!');
+    });
+})
+.catch(console.error);
